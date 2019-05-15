eedf6d70cfbba02b2398dad0a33fa33cbbf49b77
ffhelicopter <roteman@163.com>
2019-05-15 21:38:43 +0800

    更新0515

--- source-code/ch03/2/main.go
+++ source-code/ch03/2/main.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func main() {  
-    x := [3]int{1, 2, 3}
+func main() {
+	x := [3]int{1, 2, 3}
 
-    func(arr *[3]int) {
-        (*arr)[0] = 7
-        fmt.Println(arr) // prints &[7 2 3]
-    }(&x)
+	func(arr *[3]int) {
+		(*arr)[0] = 7
+		fmt.Println(arr) // prints &[7 2 3]
+	}(&x)
 
-    fmt.Println(x) // prints [7 2 3]
+	fmt.Println(x) // prints [7 2 3]
 }
